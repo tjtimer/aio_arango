11b32bfc1d3a430a6f58620a57930db23daeefa0
tjtimer <tjtimer@gmail.com>
2018-07-06 08:10:37 +0200

    besser

--- tests/docs_to_urls.py
+++ tests/docs_to_urls.py
@@ -13,29 +13,26 @@ method_re = re.compile(r'[GOSUPCLTHEAD]+')
 code_re = re.compile(r'({(?P<obj>\w+)-(?P<key>\w+)*\})')
 
 def serialize(content: str):
+    def get_name(method, url):
+        parts = [p for p in url.split('/')
+                 if not any([sym in p for sym in ['_', '{']])]
+        return parts[0] + '_'.join(parts[1:]) + f"_{method.lower()}"
     target = {}
     cont = BeautifulSoup(content, 'lxml-xml')
     for h3 in cont.find_all('h3', id=True):
-        name = h3['id'].replace(
-                '-their-', '-'
-        ).replace(
-                '-the-', '-'
-        ).replace(
-                '-a-', '-'
-        ).replace(
-                '-of-', '-'
-        ).replace(
-                '-', '_'
-        ).replace('returns_', '')
-        target[name] = {}
+        desc = h3['id']
         try:
             base = h3.find_next('code')
             m, url = base.string.split(' ')
-            target[name].update({'url': url,
-                                 'method': m,
-                                 'required': [],
-                                 'options': []}
-                                )
+            name = get_name(m, url)
+            print(name)
+            target[name] = {
+                'url': url,
+                'method': m,
+                'required': [],
+                'options': [],
+                'desc': desc
+            }
             ulist = h3.find_next('ul')
             last = ''
             for s in ulist.strings:
@@ -56,7 +53,7 @@ def parse():
         if in_file.parent.name not in config.keys():
             config[in_file.parent.name] = {}
         with in_file.open('r') as docs:
-            config[in_file.parent.name][in_file.name.split('.')[0]] = serialize(docs.read())
+            config[in_file.parent.name].update(**serialize(docs.read()))
     pprint(config)
     with open('url_conf.yaml', 'w') as conf:
         yaml.dump(config, conf)
