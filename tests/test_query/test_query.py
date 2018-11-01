9d487899711376f26a647e32e0163c459d109cb3
tjtimer <tjtimer@gmail.com>
2018-11-01 15:18:28 +0100

    test query beginn

--- tests/test_query/test_query.py
+++ tests/test_query/test_query.py
@@ -1,10 +1,11 @@
 from aio_arango.db import ArangoDB
+from aio_arango.query import query
 
 
 async def test_query():
     async with ArangoDB('root', 'arango-pw', '_system') as db:
         counter = 0
-        async for obj in db.query("FOR i IN 1..5000 RETURN {'idx': i, 'sqr': i POW 2}"):
+        async for obj in query(db, "FOR i IN 1..5000 RETURN {'idx': i, 'sqr': i POW 2}"):
             assert 'idx' in obj.keys()
             assert 'sqr' in obj.keys()
             assert int(obj['idx'])**2 == int(obj['sqr'])
