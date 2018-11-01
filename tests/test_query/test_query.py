3f44bba696e94f730303afdced252bb2d229a8a9
tjtimer <tjtimer@gmail.com>
2018-11-01 14:35:22 +0100

    test query beginn

--- tests/test_query/test_query.py
+++ tests/test_query/test_query.py
@@ -2,7 +2,7 @@ from aio_arango.db import ArangoDB
 
 
 async def test_query():
-    async with ArangoDB('root', 'arango-pw') as db:
+    async with ArangoDB('root', 'arango-pw', '_system') as db:
         counter = 0
         async for obj in db.query("FOR i IN 1..5000 RETURN {'idx': i, 'sqr': i POW 2}"):
             assert 'idx' in obj.keys()
